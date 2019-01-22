7dfcd4085c016a93a33144954e4563e7567e0d5c
VOLUNTIS\willy.dongmo <willy.dongmo@voluntis.com>
2019-01-22 14:58:51 +0100

    Updates

---
+++ precache-manifest.b9782baa6f5ca7dcae22a78ab61321c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "036993615acbccafb03f",
+    "url": "/robotfriends/static/js/runtime~main.03699361.js"
+  },
+  {
+    "revision": "ecb5d505b1c842e075ad",
+    "url": "/robotfriends/static/js/main.ecb5d505.chunk.js"
+  },
+  {
+    "revision": "15f7dac14a71143ba361",
+    "url": "/robotfriends/static/js/1.15f7dac1.chunk.js"
+  },
+  {
+    "revision": "ecb5d505b1c842e075ad",
+    "url": "/robotfriends/static/css/main.30da31c6.chunk.css"
+  },
+  {
+    "revision": "15f7dac14a71143ba361",
+    "url": "/robotfriends/static/css/1.51682531.chunk.css"
+  },
+  {
+    "revision": "7c23a06bfa24c326bc3944d8ed3f8ac8",
+    "url": "/robotfriends/index.html"
+  }
+];
